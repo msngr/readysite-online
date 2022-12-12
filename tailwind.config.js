dd3bbd1ab25328a8af8751090ad4cf0641164061
Sergej Maisinger <sergej.maisinger@live.de>
2022-12-12 15:07:18 +0700

    add breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,26 @@ module.exports = {
         './app.vue',
     ],
     theme: {
-        extend: {},
+        screens: {
+            a: '360px', // androids
+            i: '375px', // iPhones
+            n: '412px', // 412/414px new phones
+            b: '480px', // big phones
+            sm: '640px',
+            md: '768px',
+            lg: '1024px',
+            xl: '1280px',
+            '2xl': '1440px',
+            '5xl': '1920px',
+        },
+        extend: {
+            fontFamily: {
+                sans: ['Roboto', 'sans', 'ui-font-family'],
+            },
+            maxWidth: {
+                '2xl': '1440px',
+                '5xl': '1920px',
+            },
+        },
     },
-    plugins: [],
 }
