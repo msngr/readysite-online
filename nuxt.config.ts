0bcc995a6715070b9798cca6c945ae86e4159b77
Sergej Maisinger <sergej.maisinger@live.de>
2022-12-12 15:04:57 +0700

    add header tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    app: {
+        head: {
+            charset: 'utf-8',
+            viewport: 'width=device-width, initial-scale=1',
+            title: 'readysite.online',
+            meta: [
+                {
+                    name: 'description',
+                    content: 'Get your ready website today!',
+                },
+            ],
+            // script: [
+            //     {
+            //         src: 'https://cdn.jsdelivr.net/npm/tw-elements/dist/js/index.min.js',
+            //         defer: true,
+            //     },
+            // ],
+            // link: [{ rel: 'stylesheet', href: 'https://awesome-lib.css' }],
+            noscript: [{ children: 'Javascript is required' }],
+        },
+    },
     css: ['~/assets/css/main.css'],
     postcss: {
         plugins: {
