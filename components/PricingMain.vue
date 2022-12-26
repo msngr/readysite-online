37655493cdda079f8f3124ebf122560c31514a81
Sergej Maisinger <sergej.maisinger@live.de>
2022-12-26 14:12:56 +0700

    finish pricing section

--- components/PricingMain.vue
+++ components/PricingMain.vue
@@ -1,6 +1,6 @@
 <template>
-    <!-- anchor tag -->
-    <span class="anchor" id="Preise"></span>
+    <!-- anchor pricing -->
+    <span class="anchor !-top-12 md:!top-0" id="Preise"></span>
     <!-- Background-Gradient -->
     <section class="bg-gradient-to-tr from-white via-sky-50 to-sky-100">
         <div
@@ -8,7 +8,7 @@
         >
             <div class="xl:items-center xl:flex 5xl:ml-10">
                 <div
-                    class="flex flex-col items-center xl:items-start xl:mr-14 5xl:mx-8"
+                    class="flex flex-col items-center xl:items-start xl:min-w-[20rem] xl:max-w-md mx-2 b:mx-10 md:mx-6 lg:mx-12 xl:mx-6 5xl:mx-8"
                 >
                     <!-- Headline -->
                     <h1
@@ -18,9 +18,23 @@
                         <span class="text-gradient">Preise </span>
                     </h1>
                     <DividerGradient />
-                    <div class="mt-4 mx-4 n:mx-0 text-gray-500">
-                        <p>Text.</p>
-                        <p>Und mehr Text.</p>
+                    <div class="mt-10 mx-4 mb-2 n:mx-0 text-gray-600">
+                        <p>
+                            Wählen Sie eine Website-Vorlage oder unser
+                            Hosting-Paket und bezahlen Sie bequem mit Paypal
+                            oder Kreditkarte im nächsten Schritt.
+                        </p>
+                        <br />
+                        <p>
+                            Für spezielle Anfragen, Beratung oder Anpassungen
+                            <a
+                                href="#Kontakt"
+                                class="inline text-sky-600 hover:underline"
+                                >kontaktieren</a
+                            >
+                            Sie uns. Wir berechnen Ihnen gerne ein individuelles
+                            Angebot, je nach Zeitaufwand.
+                        </p>
                     </div>
                 </div>
                 <div class="flex-1 xl:mx-8">
@@ -29,7 +43,7 @@
                     >
                         <!-- Website Pricing -->
                         <div
-                            class="max-w-sm mx-auto shadow-md rounded-lg md:mx-4"
+                            class="max-w-sm xl:w-96 mx-auto shadow-md rounded-lg md:mx-4"
                         >
                             <div class="p-6">
                                 <h2
@@ -40,13 +54,17 @@
 
                                 <p class="mt-4 text-gray-500">
                                     Vorlage auswählen, Inhalte zusenden und
-                                    innerhalb von 3 Werktagen Website erhalten.
+                                    innerhalb von 3 Werktagen Ihre fertige
+                                    Website erhalten.
                                 </p>
 
                                 <p
                                     class="mt-4 text-2xl font-medium text-gray-700 sm:text-4xl"
                                 >
-                                    299.00€
+                                    299€<span
+                                        class="inline-block text-base -translate-y-[0.9rem]"
+                                        >1</span
+                                    >
                                 </p>
 
                                 <p class="mt-1 text-gray-500">
@@ -56,7 +74,7 @@
                                 <a
                                     class="text-white text-lg font-medium inline-flex items-center shadow-md bg-gradient-to-r from-sky-500 to-sky-700 rounded-full hover:from-sky-600 hover:to-sky-900 transition py-3 px-5 mt-7 -ml-1.5"
                                     href="/#Website_wählen"
-                                    >Jetzt bestellen
+                                    >Jetzt bestellen<sup>2</sup>
                                 </a>
                             </div>
 
@@ -87,7 +105,7 @@
                         </div>
                         <!-- Hosting Pricing -->
                         <div
-                            class="max-w-sm mx-auto shadow-md rounded-lg md:mx-4"
+                            class="max-w-sm xl:w-96 mx-auto shadow-md rounded-lg md:mx-4"
                         >
                             <div class="p-6">
                                 <h1
@@ -97,14 +115,18 @@
                                 </h1>
 
                                 <p class="mt-4 text-gray-500">
-                                    Bringen Sie Ihre Website online, und lassen
-                                    Sie uns die technische Seite übernehmen.
+                                    Bringen Sie Ihre Website online und lassen
+                                    Sie uns die technische und rechtliche Seite
+                                    übernehmen.
                                 </p>
 
                                 <p
                                     class="mt-4 text-2xl font-medium text-gray-700 sm:text-4xl"
                                 >
-                                    119.00€
+                                    119€<span
+                                        class="inline-block text-base -translate-y-[0.9rem]"
+                                        >1</span
+                                    >
                                 </p>
 
                                 <p class="mt-1 text-gray-500">
@@ -114,7 +136,7 @@
                                 <a
                                     class="text-white text-lg font-medium inline-flex items-center shadow-md bg-gradient-to-r from-sky-500 to-sky-700 rounded-full hover:from-sky-600 hover:to-sky-900 transition py-3 px-5 mt-7 -ml-1.5"
                                     href="/#Website_wählen"
-                                    >Jetzt bestellen
+                                    >Jetzt bestellen<sup>2</sup>
                                 </a>
                             </div>
 
@@ -147,6 +169,72 @@
                         </div>
                     </div>
                 </div>
+            </div>
+            <div
+                class="shadow-md rounded max-w-2xl px-5 2xl:mx-8 5xl:mx-14 my-8"
+            >
+                <p class="font-medium"><sup>1</sup> Ergänzende Hinweise:</p>
+                <ul class="py-4 space-y-2 ml-3">
+                    <li>
+                        Alle Preise sind Endpreise und enthalten die gesetzlich
+                        vorgeschriebene Umsatzsteuer, die Ihnen auf Ihrer
+                        Rechnung ausgewiesen wird.
+                    </li>
+                    <li>
+                        Sollte keine Umsatzsteuer vorgeschrieben sein oder das
+                        Reverse-Charge-Verfahren zum Einsatz kommen, so sind
+                        dieselben Preise als Nettopreise zu verstehen.
+                    </li>
+
+                    <p class="font-medium pt-4">
+                        <sup>2</sup> Bitte bestätigen:
+                    </p>
+
+                    <div class="flex pt-2">
+                        <input
+                            id="checkbox1"
+                            type="checkbox"
+                            value=""
+                            class="w-4 h-4 xl:min-w-[1rem]"
+                            style="accent-color: #0284c7"
+                        />
+                        <label for="checkbox1" class="ml-2"
+                            >Ich habe die obigen ergänzenden Hinweise zur
+                            Kenntnis genommen und stimme den
+                            <a
+                                href="#"
+                                class="inline text-sky-600 hover:underline"
+                                >AGB</a
+                            >, der
+                            <a
+                                href="#"
+                                class="inline text-sky-600 hover:underline"
+                                >Datenschutzerklärung</a
+                            >, sowie der
+                            <a
+                                href="#"
+                                class="inline text-sky-600 hover:underline"
+                                >Widerrufsbelehrung
+                            </a>
+                            zu.</label
+                        >
+                    </div>
+                    <div class="flex pt-2">
+                        <input
+                            id="checkbox2"
+                            type="checkbox"
+                            value=""
+                            class="w-4 h-4 xl:min-w-[1rem]"
+                            style="accent-color: #0284c7"
+                        />
+                        <label for="checkbox2" class="ml-2"
+                            >Ich stimme ausdrücklich zu, dass vor Ablauf der
+                            Widerrufsfrist mit der Ausführung des Vertrags
+                            begonnen wird. Mir ist bekannt, dass mit Beginn der
+                            Ausführung mein Widerrufsrecht erlischt.</label
+                        >
+                    </div>
+                </ul>
             </div>
         </div>
     </section>
