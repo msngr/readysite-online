c72c943166c3c6efe7c5fb909dec202716b5d865
Sergej Maisinger <sergej.maisinger@live.de>
2022-12-28 18:15:22 +0700

    larger shadow on navbar

--- components/NavbarMain.vue
+++ components/NavbarMain.vue
@@ -1,6 +1,6 @@
 <template>
     <nav
-        class="navbar z-40 fixed bg-sky-50 w-full max-w-5xl mx-auto flex flex-wrap items-center justify-between px-4 sm:px-6 md:px-8 py-3 sm:py-4 shadow-md"
+        class="navbar z-40 fixed bg-sky-50 w-full max-w-5xl mx-auto flex flex-wrap items-center justify-between px-4 sm:px-6 md:px-8 py-3 sm:py-4 shadow-xl"
     >
         <!-- Logo and Menu Container -->
         <div
